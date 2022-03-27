a7bc050ee1c0b7c0f6e72011ad07c97c2c9e12f2
Joshua Prince <joshua@jtprince.com>
2022-03-27 16:53:22 -0700

    Clean up code that was moved to -MC project

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Collect static files
-# Really not necessary without a frontend
-#echo "Collect static files"
-#python ./manage.py collectstatic --noinput
-
 # Apply database migrations
 echo "Apply database migrations"
 python ./manage.py migrate
